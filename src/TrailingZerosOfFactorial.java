a878f4d9d5749116415caca1466b251aaa68d223
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-11 20:58:35 -0700

    added the comments for better grip

--- src/TrailingZerosOfFactorial.java
+++ src/TrailingZerosOfFactorial.java
@@ -1,6 +1,10 @@
 public class TrailingZerosOfFactorial {
     public static int trailingZerosOfFactorial(int n){
+        //initialize the result with 0 first
         int result =0;
+        // run a loop until n gets updated to 0
+        //all we need is the multiples of 5, and then add multiple of 5 until it becomes 0
+        //for ex. 25. 25/5=5, result =5, then second iteration will have 5/5=1, result =5+1=6
         while(n>0){
             n=n/5;
             result +=n;
