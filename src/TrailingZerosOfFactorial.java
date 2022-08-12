5aa89dc5481b06b3b96075f78a51f2c190dc8df5
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-11 20:56:05 -0700

    coded the method to return the number of zeros(trailing) when we take a factorial of it

--- src/TrailingZerosOfFactorial.java
+++ src/TrailingZerosOfFactorial.java
@@ -1,5 +1,12 @@
 public class TrailingZerosOfFactorial {
+    public static int trailingZerosOfFactorial(int n){
+        int result =0;
+        while(n>0){
+            n=n/5;
+            result +=n;
+        }return result;
+    }
     public static void main(String[] args) {
-        
+
     }
 }
